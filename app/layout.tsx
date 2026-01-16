60653f26ce7d2704f12a16197bccdcff429dd853
raptor7197 <vamsikrishna75836@gmail.com>
2026-01-16 13:22:07 +0530

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,67 @@
+import type { Metadata } from "next";
+import { 
+  Space_Grotesk, 
+  Urbanist, 
+  Poppins, 
+  Roboto_Slab, 
+  Angkor, 
+  Inter 
+} from "next/font/google";
+import "./globals.css";
+
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const urbanist = Urbanist({
+  variable: "--font-urbanist",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+const robotoSlab = Roboto_Slab({
+  variable: "--font-roboto-slab",
+  subsets: ["latin"],
+  weight: ["400", "800"],
+});
+
+const angkor = Angkor({
+  variable: "--font-angkor",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "ITC India 2026 - IEEE International Test Conference",
+  description: "10th IEEE International Test Conference INDIA - An initiative towards India's semiconductor ecosystem",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${spaceGrotesk.variable} ${urbanist.variable} ${poppins.variable} ${robotoSlab.variable} ${angkor.variable} ${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
